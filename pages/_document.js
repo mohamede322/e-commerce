3ef7ac6ab72b628c6ba841759b510dd6f941526f
Mohamed Essam <93525954+mohamede322@users.noreply.github.com>
2023-08-17 17:27:45 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,6 @@ export default function Document() {
     <Html>
       <Head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
-        <link
-          rel="icon"
-          href="https://media.istockphoto.com/vectors/shopping-cart-icon-vector-id1128229893?k=20&m=1128229893&s=612x612&w=0&h=uOQYRr-vTDnW60Mn8MWSwt6i9uK2SGni8jR1CKKELK8="
-        />
       </Head>
       <body>
         <Main />
