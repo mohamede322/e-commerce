0f4d81dee8df22a5bbe11687ea032a85de9b90ac
Mohamed Essam <93525954+mohamede322@users.noreply.github.com>
2023-08-17 17:32:38 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -28,11 +28,7 @@ function MyApp({ Component, pageProps }) {
         </Layout>
       </ctx.Provider>
 
-      <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossOrigin="anonymous"
-      ></Script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     </>
   );
 }
