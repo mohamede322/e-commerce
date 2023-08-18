f4f1623e1f970690afc07da14f2ecf61f880a679
Mohamed Essam <93525954+mohamede322@users.noreply.github.com>
2023-08-18 17:44:30 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,7 @@ export default function Document() {
       <Head>
         <link
           rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossOrigin="anonymous"
+          href="./css/bootstrap.min.css"
         ></link>
         <link
           rel="icon"
