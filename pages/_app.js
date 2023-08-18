b90bd78104168eb20ac8051816293f639f4efe9b
Mohamed Essam <93525954+mohamede322@users.noreply.github.com>
2023-08-18 17:44:22 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -14,8 +14,6 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta charSet="utf-8" />
         <title>E-Commerce</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
-
       </Head>
       <ctx.Provider
         value={{
@@ -30,7 +28,10 @@ function MyApp({ Component, pageProps }) {
         </Layout>
       </ctx.Provider>
 
-<Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+      <Script
+            src="./js/bootstrap.bundle.min.js"
+      ></Script>
+    </>
   );
 }
 
